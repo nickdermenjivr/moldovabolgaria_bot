8d9b96fd4b45fbc66ed99af93e50826bd8561e8b
nickdermenjivr <nickdermenji@gmail.com>
2025-03-20 14:23:26 +0200

    introduction

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("8181148069:AAHGwZXVK1rLdQ45g5D8_KCf2BPqZ1Q_IkE", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnError += OnError;
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter 'stop' to terminate");
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input?.ToLower() == "stop")
+    {
+        cts.Cancel();
+        break;
+    }
+    Console.WriteLine("No such command!");
+}
+
+// method to handle errors in polling or in your OnMessage/OnUpdate code
+Task OnError(Exception exception, HandleErrorSource source)
+{
+    Console.WriteLine(exception); // just dump the exception to the console
+    return Task.CompletedTask;
+}
+
+// method that handle messages received by the bot:
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text == "/start")
+    {
+        await bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
+            replyMarkup: new InlineKeyboardButton[] { "Left", "Middle", "Right" });
+    }
+}
+
+// method that handle other types of updates received by the bot:
+async Task OnUpdate(Update update)
+{
+    if (update is { CallbackQuery: { } query }) // non-null CallbackQuery
+    {
+        await bot.AnswerCallbackQuery(query.Id);
+        await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+    }
+}
